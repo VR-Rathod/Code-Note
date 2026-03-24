12054fd3d7462eaab44f53e328d2cbb453f766b2
VR-Rathod <vaibhavrathod2282@gmail.com>
2026-03-24 18:10:01 +0530

    Fix URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "hi-IN",
-    baseUrl: "code-note-gamma.vercel.app",
+    baseUrl: "code-note-VR-Rathod.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
